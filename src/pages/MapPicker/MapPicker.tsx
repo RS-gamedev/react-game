cdedaa58533697a2bf261f0e824d04b3595d548d
Sander van Maurik <s.vanmaurik@jasbit.nl>
2022-11-25 16:58:27 +0100

    Added slider WIP

--- src/pages/MapPicker/MapPicker.tsx
+++ src/pages/MapPicker/MapPicker.tsx
@@ -3,6 +3,14 @@ import Button from "../../components/Button/Button";
 import { MapPickerObject } from "../../models/MapPickerObject";
 import { generateMap } from "../../utils/MapUtils";
 import styles from "./MapPicker.module.css";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+
+type CustomizeOption = {
+  mapObject: MapPickerObject;
+  value: number;
+};
+
 type Props = {
   onStart: (objects: MapPickerObject[]) => void;
   mapSize: { height: number; width: number };
@@ -10,11 +18,27 @@ type Props = {
 
 export default function MapPicker({ onStart, mapSize }: Props) {
   const [mapObjects, setMapObjects] = useState<MapPickerObject[]>();
+  const [customizeOptions, setCustomizeOptions] = useState<CustomizeOption[]>([
+    {
+      mapObject: {
+        color: "green",
+        name: "Tree",
+        previewSize: { width: 200, height: 50 },
+      },
+      value: 50,
+    },
+    {
+      mapObject: { color: "grey", name: "Rock", previewSize: { width: 200, height: 50 } },
+      value: 50,
+    },
+  ]);
+  const [triggerGenerate, setTriggerGenerate] = useState(false);
+
   const settingsWidth = 240;
 
   useEffect(() => {
-    setMapObjects(generateMap(mapSize.height - 60, 200, 50));
-  }, []);
+    generateNewMap();
+  }, [triggerGenerate]);
 
   const onClick = () => {
     console.log(mapObjects);
@@ -22,29 +46,42 @@ export default function MapPicker({ onStart, mapSize }: Props) {
   };
 
   const generateNewMap = () => {
-    setMapObjects(generateMap(mapSize.height - 60, 200, 50));
+    let tree = customizeOptions.find((x) => x.mapObject.name === "Tree");
+    let rock = customizeOptions.find((x) => x.mapObject.name === "Rock");
+    if (!tree || !rock) return;
+    setMapObjects((prev) => generateMap(mapSize.height - 60, tree!.value, rock!.value));
   };
 
-  const mapItems: MapPickerObject[] = [
-    {
-      color: "green",
-      name: "Tree",
-      previewSize: { width: 200, height: 50 },
-    },
-    { color: "grey", name: "Rock", previewSize: { width: 200, height: 50 } },
-  ];
+  function changeValue(event: any, option: CustomizeOption) {
+    setCustomizeOptions((prev) => {
+      return prev.map((x) => {
+        if (x.mapObject.name === option.mapObject.name) {
+          return { ...x, value: event };
+        }
+        return { ...x };
+      });
+    });
+  }
+
+  function afterChange() {
+    setTriggerGenerate((prev) => !prev);
+  }
 
   return (
     <div className={styles.background}>
       <div className={styles.settingsArea} style={{ width: settingsWidth }}>
         <div className={styles.settingsCard}>
           <div className={styles.legend}>
-            {mapItems.map((x) => {
+            {customizeOptions.map((x) => {
               return (
-                <div className={styles.legendItem} style={{ width: x.previewSize.width, height: x.previewSize.height }}>
-                  <div style={{ background: x.color }}></div>
-                  <span>{x.name}</span>
-                </div>
+                // <div className={styles.legendItem} style={{ width: x.previewSize.width, height: x.previewSize.height }}>
+                //   <div style={{ background: x.color }}></div>
+                //   <span>{x.name}</span>
+                // </div>
+                <>
+                  {/* <div style={{ background: x.mapObject.color }}></div> */}
+                  <Slider min={0} max={500} value={x.value} onAfterChange={afterChange} onChange={(event) => changeValue(event, x)}></Slider>
+                </>
               );
             })}
           </div>
