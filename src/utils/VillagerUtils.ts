bbd9b490bf4aa74aba1dba64de3e214bcd1f8f49
Sander van Maurik <s.vanmaurik@jasbit.nl>
2022-11-27 17:55:10 +0100

    Inventory wood and stone bug fix

--- src/utils/VillagerUtils.ts
+++ src/utils/VillagerUtils.ts
@@ -1,7 +1,11 @@
+import { Resource } from "../models/Resource";
 import { VillagerProps } from "../models/VillagerProps";
+import { add } from "./ResourceUtils";
 
 export function inventoryIsFull(villager: VillagerProps): boolean {
-  if (villager.inventoryItems[0] && villager.inventoryItems[0].amount > villager.inventorySlots) {
+  let totalInInventory = 0;
+  villager.inventoryItems.forEach((x) => (totalInInventory = add(totalInInventory, x.amount)));
+  if (totalInInventory > villager.inventorySlots) {
     return true;
   }
   return false;
